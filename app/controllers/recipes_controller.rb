7bef3a22000caf20e6f6b6fc124999efb7301ebb
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-24 14:56:17 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,4 +21,15 @@ class RecipesController < ApplicationController
      render :new
    end
   end
+
+  private
+     def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+        ]
+      )
+    end
 end
